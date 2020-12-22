63a75f55fffcb054566143142de850d8e52e2f11
ryanPfaffman <73614186+ryanPfaffman@users.noreply.github.com>
2020-12-22 18:04:51 -0500

    hello files

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace quizGui.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            /*
+            ViewData["Question1"] = new Question {
+                Question = "\nC# programmers use Pascal casting when creating method names since this convention is required to produce a workable program\n\tA. True\n\tB. False",
+                Answer = "a"
+            };
+            ViewData["Question2"] = new Question {
+                Question = "\nThe C# programming language was developed as an object and compound-oriented language\n\tA. True\n\tB. False",
+                Answer = "a"
+            };
+            ViewData["Question3"] = new Question {
+                Question = "\nWhen the keyword Void is used in the Main() method header, it indicates that the Main() method is empty\n\tA. True\n\tB. False",
+                Answer = "b"
+            };
+            ViewData["Question4"] = new Question {
+                Question = "\nYou can use the /out Compiler Option between the csc command and the name of the .cs file to indicate the name of the output file\n\tA. True\n\tB. False",
+                Answer = "b"
+            };
+            ViewData["Question5"] = new Question {
+                Question = "\nFlowchart creators use diamond shapes to indicate alternative courses of action\n\tA. True\n\tB. False",
+                Answer = "a"
+            };
+            */
+
+            ViewData["Question1"] = "C# programmers use Pascal casting when creating method names since this convention is required to produce a workable program";
+            ViewData["Question2"] = "The C# programming language was developed as an object and compound-oriented language";
+            ViewData["Question3"] = "When the keyword Void is used in the Main() method header, it indicates that the Main() method is empty";
+            ViewData["Question4"] = "You can use the /out Compiler Option between the csc command and the name of the .cs file to indicate the name of the output file";
+            ViewData["Question5"] = "Flowchart creators use diamond shapes to indicate alternative courses of action";
+
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,52 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const button = document.getElementById("button");
+const numCorrect1 = document.getElementById("numCorrect1");
+
+let numCorrect = 0;
+
+let allQuestions = false;
+
+const quizOver = () => {
+    alert("Quiz Over");
+}
+
+const quiz = () => {
+    if (document.querySelector('input[name="question1"]:checked') === null || document.querySelector('input[name="question2"]:checked') === null || document.querySelector('input[name="question3"]:checked') === null || document.querySelector('input[name="question4"]:checked') === null || document.querySelector('input[name="question5"]:checked') === null) {
+        alert("All questions not answered");
+    } else {
+        allQuestions = true;
+    }
+    
+    if (document.querySelector('input[name="question1"]:checked').value == 'null' && allQuestions === true) {
+        alert("Not all question answered");
+    }
+    if (document.querySelector('input[name="question1"]:checked').value === "t" && allQuestions === true) {
+        numCorrect++;
+    }
+    if (document.querySelector('input[name="question2"]:checked').value === "t" && allQuestions === true) {
+        numCorrect++;
+    }
+    if (document.querySelector('input[name="question3"]:checked').value === "f" && allQuestions === true) {
+        numCorrect++;
+    }
+    if (document.querySelector('input[name="question4"]:checked').value === "f" && allQuestions === true) {
+        numCorrect++;
+    }
+    if (document.querySelector('input[name="question5"]:checked').value === "t" && allQuestions === true) {
+        numCorrect++;
+    }
+
+    if (allQuestions === true) {
+        button.removeEventListener('click', quiz);
+        button.addEventListener('click', quizOver);
+    }
+
+    numCorrect1.innerHTML = "Correct: " + numCorrect.toString() + "<br>" 
+    + "Questions: 5<br>" 
+    + "Grade: " + Math.round(numCorrect / 5 * 100).toString() + "%";
+}
+
+button.addEventListener('click', quiz);
