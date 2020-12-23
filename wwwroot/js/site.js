0dabe37874136b32b23b0f297442d88a83c8356c
ryanPfaffman <73614186+ryanPfaffman@users.noreply.github.com>
2020-12-23 18:19:33 -0500

    changed css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -13,6 +13,11 @@ const ansQ5 = document.getElementById("ansQ5").innerHTML;
 const ansQ6 = document.getElementById("ansQ6").innerHTML;
 const ansQ7 = document.getElementById("ansQ7").innerHTML;
 const ansQ8 = document.getElementById("ansQ8").innerHTML;
+const ansQ9 = document.getElementById("ansQ9").innerHTML;
+const ansQ10 = document.getElementById("ansQ10").innerHTML;
+const ansQ11 = document.getElementById("ansQ11").innerHTML;
+const essay = document.getElementById("essay");
+const form = document.getElementById("formQuizGui");
 
 let numCorrect = 0;
 
@@ -23,12 +28,13 @@ const quizOver = () => {
 }
 
 const quiz = () => {
-    if (document.querySelector('input[name="question1"]:checked') === null || document.querySelector('input[name="question2"]:checked') === null || document.querySelector('input[name="question3"]:checked') === null || document.querySelector('input[name="question4"]:checked') === null || document.querySelector('input[name="question5"]:checked') === null || document.querySelector('input[name="question6"]:checked') === null || document.querySelector('input[name="question7"]:checked') === null || document.querySelector('input[name="question8"]:checked') === null) {
+    let essayVal = essay.value;
+    if (document.querySelector('input[name="question1"]:checked') === null || document.querySelector('input[name="question2"]:checked') === null || document.querySelector('input[name="question3"]:checked') === null || document.querySelector('input[name="question4"]:checked') === null || document.querySelector('input[name="question5"]:checked') === null || document.querySelector('input[name="question6"]:checked') === null || document.querySelector('input[name="question7"]:checked') === null || document.querySelector('input[name="question8"]:checked') === null || document.querySelector('input[name="question9"]:checked') === null || document.querySelector('input[name="question10"]:checked') === null || document.querySelector('input[name="question11"]:checked') === null) {
         alert("All questions not answered");
     } else {
         allQuestions = true;
     }
-    
+
     if (document.querySelector('input[name="question1"]:checked').value === ansQ1 && allQuestions === true) {
         numCorrect++;
     }
@@ -53,15 +59,25 @@ const quiz = () => {
     if (document.querySelector('input[name="question8"]:checked').value === ansQ8 && allQuestions === true) {
         numCorrect++;
     }
+    if (document.querySelector('input[name="question9"]:checked').value === ansQ9 && allQuestions === true) {
+        numCorrect++;
+    }
+    if (document.querySelector('input[name="question10"]:checked').value === ansQ10 && allQuestions === true) {
+        numCorrect++;
+    }
+    if (document.querySelector('input[name="question11"]:checked').value === ansQ11 && allQuestions === true) {
+        numCorrect++;
+    }
+
+    numCorrect1.innerHTML = "Correct: " + numCorrect.toString() + "<br>" 
+    + "Questions: 11<br>" 
+    + "Grade: " + Math.round(numCorrect / 11 * 100).toString() + "%";
 
     if (allQuestions === true) {
         button.removeEventListener('click', quiz);
         button.addEventListener('click', quizOver);
+        formQuizGuiz.submit();
     }
-
-    numCorrect1.innerHTML = "Correct: " + numCorrect.toString() + "<br>" 
-    + "Questions: 8<br>" 
-    + "Grade: " + Math.round(numCorrect / 8 * 100).toString() + "%";
 }
 
 button.addEventListener('click', quiz);
